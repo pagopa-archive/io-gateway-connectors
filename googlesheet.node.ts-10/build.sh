8c53a53a027fcbc8562818a5a12b81a314d75911
sf3ris <vale.piz92@hotmail.it>
2020-12-11 22:23:06 +0100

    Added build configuration parameters on gh-pages

--- googlesheet.node.ts-10/build.sh
+++ googlesheet.node.ts-10/build.sh
@@ -10,7 +10,7 @@ then echo "preparing archive, please wait..."
      zip -qr $ZIP_FILE node_modules
 fi
 npm run tsc
-cd dist
+cd dist || { echo "dist directory not found, aborting..."; exit 1; }
 zip -u ../$ZIP_FILE *.js -x *.test.js
 cd ..
 #wsk action update iosdk/import index.zip --kind nodejs:10 --web true
